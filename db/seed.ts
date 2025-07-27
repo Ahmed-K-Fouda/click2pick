f1f35db800a554548f0ad37bea995932af75a49d
A.Fouda <ahmedfoudahany@gmail.com>
2025-07-27 12:05:37 +0300

    add user data into prisma studio

--- db/seed.ts
+++ db/seed.ts
@@ -3,7 +3,17 @@ import sampleData from "./sample-data";
 
 async function main() {
   const prisma = new PrismaClient();
+  // seed product
   await prisma.product.deleteMany();
+
   await prisma.product.createMany({ data: sampleData.products });
+
+  // seed user
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
+  await prisma.user.createMany({ data: sampleData.users });
 }
 main();

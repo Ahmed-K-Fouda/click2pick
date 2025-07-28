de5769dc909fd5085cffb09da8f212bd2ff3ac13
A.Fouda <ahmedfoudahany@gmail.com>
2025-07-28 12:40:26 +0300

    update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { ZodError } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -15,4 +16,26 @@ export function convertToPlainObj<T>(value: T): T {
 export function formatNumberWithDecimal(val: number): string {
   const [int, dec] = val.toString().split(".");
   return dec ? `${int}.${dec.padEnd(2, "0")}` : `${int}.00`;
+}
+
+// format error
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function formatError(error: any) {
+  if (error instanceof ZodError) {
+    return error.issues.map((e) => e.message).join(". ");
+  }
+
+  if (
+    error?.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return `${
+      field.charAt(0).toUpperCase() + field.slice(1)
+    } is already registered`;
+  }
+
+  return typeof error.message === "string"
+    ? error.message
+    : JSON.stringify(error.message);
 }

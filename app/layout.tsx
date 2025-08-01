8de584ea25271c82038a06c49c59af97e5697992
A.Fouda <ahmedfoudahany@gmail.com>
2025-08-01 18:15:37 +0300

    feat: add toaster to the layou

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({
   subsets: ["latin"],
 });
@@ -31,6 +32,7 @@ export default function RootLayout({
           defaultTheme="light"
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>

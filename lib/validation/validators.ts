ddbe4a89595da029d19fb512ff2d0b5ec9522b6b
A.Fouda <ahmedfoudahany@gmail.com>
2025-08-01 18:18:32 +0300

    update: add cartItemSchema &  insertCartSchema

--- lib/validation/validators.ts
+++ lib/validation/validators.ts
@@ -41,3 +41,24 @@ export const signUpFormSchema = z
     error: "Passwords don't match",
     path: ["confirmPassword"],
   });
+
+// CART ITEM SCHEMA
+
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, "Product is required"),
+  name: z.string().min(1, "Name is required"),
+  slug: z.string().min(1, "Slug is required"),
+  qty: z.number().int().nonnegative("Product must me positive number"),
+  image: z.string().min(1, "Image is required"),
+  price: currecnyFormatted,
+});
+
+export const insertCartSchema = z.object({
+  items: z.array(cartItemSchema),
+  itemPrice: currecnyFormatted,
+  totalPrice: currecnyFormatted,
+  shippingPrice: currecnyFormatted,
+  taxPrice: currecnyFormatted,
+  sessionCartId: z.string().min(1, "Session cart id is required"),
+  userId: z.string().optional().nullable(),
+});

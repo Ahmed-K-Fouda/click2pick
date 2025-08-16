42b208f15614003649ee5779c56590cac1fe3665
A.Fouda <ahmedfoudahany@gmail.com>
2025-08-16 11:40:59 +0300

    update: add formUrlQuery fun

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { ZodError } from "zod";
+import qs from "query-string";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -113,3 +114,27 @@ export const formatDateTime = (dateString: Date) => {
     timeOnly: formattedTime,
   };
 };
+
+// form the pagination links
+export function formUrlQuery({
+  params,
+  key,
+  value,
+}: {
+  params: string;
+  key: string;
+  value: string | null;
+}) {
+  const query = qs.parse(params);
+  query[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.href,
+      query,
+    },
+    {
+      skipNull: true,
+    }
+  );
+}

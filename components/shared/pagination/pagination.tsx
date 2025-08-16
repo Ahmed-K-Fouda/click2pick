32e9cfa3f38310d954498a83498b29905cb69a34
A.Fouda <ahmedfoudahany@gmail.com>
2025-08-16 11:40:11 +0300

    initial commit

---
+++ components/shared/pagination/pagination.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+interface IProps {
+  page: number | string;
+  urlParamName?: string;
+  totalPages: number;
+}
+const Pagination = ({ page, totalPages, urlParamName }: IProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleClick = (btnType: string) => {
+    const pageValue = btnType === "next" ? Number(page) + 1 : Number(page) - 1;
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || "page",
+      value: pageValue.toString(),
+    });
+    router.push(newUrl);
+  };
+
+  return (
+    <div className="flex gap-2">
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        disabled={Number(page) <= 1}
+        onClick={() => handleClick("prev")}
+      >
+        Previous
+      </Button>
+
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-28"
+        disabled={Number(page) >= 1}
+        onClick={() => handleClick("next")}
+      >
+        Next
+      </Button>
+    </div>
+  );
+};
+
+export default Pagination;
